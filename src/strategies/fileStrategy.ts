809ee8c76745a99fce5de002fc1c7b3b506f8d97
Pedro Escumalha <pedrosimoes93@gmail.com>
2023-07-29 16:29:06 +0200

    added base json file strategy

---
+++ src/strategies/fileStrategy.ts
@@ -0,0 +1,45 @@
+import fs from "fs";
+import type { VortalsStrategy } from "./types";
+
+export type FileStrategy = VortalsStrategy & {
+    parser: (file: string) => Record<string, string>;
+    path: string;
+    environment: string | undefined;
+}
+
+export abstract class BaseFileStrategy implements FileStrategy {
+    parser: (file: string) => Record<string, string>;
+    path: string;
+    environment: string | undefined;
+
+    constructor(
+        parser: (file: string) => Record<string, string>,
+        path: string,
+        environment: string | undefined) {
+        this.parser = parser;
+        this.path = path;
+        this.environment = environment;
+    }
+
+    load(): Record<string, string> {
+        const baseFile: Record<string, string> = this.parser(fs.readFileSync(this.path, "utf8"));
+        let envFile: Record<string, string> = {};
+
+        if (this.environment) {
+            envFile = this.parser(fs.readFileSync(this.path, "utf8"));
+        }
+
+
+        // TODO: This is wrong. It needs to be mapped to a key value like.
+        return {
+            ...baseFile,
+            ...envFile,
+        };
+    }
+}
+
+export class JsonFileStrategy extends BaseFileStrategy {
+    constructor(configPath = "configs/configuration.json", environment = process.env.APP_ENV) {
+        super(JSON.parse, configPath, environment);
+    }
+}
