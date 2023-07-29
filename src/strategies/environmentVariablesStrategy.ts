5d30a466bea7af9041ceb4e0d0930f6c38df53e7
Pedro Escumalha <pedrosimoes93@gmail.com>
2023-07-29 16:58:22 +0200

    added environment variables strategy

---
+++ src/strategies/environmentVariablesStrategy.ts
@@ -0,0 +1,25 @@
+import type { VortalsStrategy } from "./types";
+import { CONFIG_DELIMITER, CONFIG_PREFIX } from "./variables";
+
+export class EnvironmentVariablesStrategy implements VortalsStrategy {
+    private configPrefix = `${CONFIG_PREFIX}${CONFIG_DELIMITER}`;
+
+    private transformKey(key: string): string {
+        return key.replace(this.configPrefix, "");
+    }
+
+    private isVortalsKey(key: string): boolean {
+        return key.indexOf(this.configPrefix) === 0;
+    }
+
+    load(): Record<string, unknown> {
+        const vortalsEnvVarsKeys = Object.keys(process.env).filter((k) => this.isVortalsKey(k));
+        const envVars: Record<string, unknown> = {};
+
+        for (const key of vortalsEnvVarsKeys) {
+            envVars[this.transformKey(key)] = process.env[key];
+        }
+
+        return envVars;
+    }
+}
---
+++ src/strategies/variables.ts
@@ -0,0 +1,2 @@
+export const CONFIG_PREFIX = "VORTALS";
+export const CONFIG_DELIMITER = "__";
