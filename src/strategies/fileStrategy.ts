0db45836ce0aab36e6c0145122332670d4c0a65b
Pedro Escumalha <pedrosimoes93@gmail.com>
2023-08-06 01:15:13 +0200

    Update fileStrategy.ts

--- src/strategies/fileStrategy.ts
+++ src/strategies/fileStrategy.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import type { VortalsStrategy } from "./types";
+import { CONFIG_DELIMITER } from "../utils";
 
 export type FileStrategy = VortalsStrategy & {
     parser: (file: string) => Record<string, unknown>;
@@ -12,6 +13,30 @@ export abstract class BaseFileStrategy implements FileStrategy {
     path: string;
     environment: string | undefined;
 
+    private mapToKeyValueObject(config: Record<string, unknown>): Record<string, unknown> {
+        const res = {} as Record<string, unknown>;
+
+        const mapToKeyValueObjectDeep = (data: Record<string, unknown> | unknown[], keyPrefix = ""): void => {
+            for (const k in data) {
+                const value = config[k as keyof typeof data];
+
+                if (Array.isArray(value)) {
+                    mapToKeyValueObjectDeep(value, keyPrefix + CONFIG_DELIMITER + k);
+                }
+
+                if (typeof value === "object" && value !== null) {
+                    mapToKeyValueObjectDeep(value as Record<string, unknown>, keyPrefix + CONFIG_DELIMITER + k);
+                }
+
+                res[keyPrefix ? keyPrefix + CONFIG_DELIMITER + k : k] = value;
+            }
+        };
+
+        mapToKeyValueObjectDeep(config);
+
+        return res;
+    }
+
     constructor(
         parser: (file: string) => Record<string, unknown>,
         path: string,
@@ -21,7 +46,7 @@ export abstract class BaseFileStrategy implements FileStrategy {
         this.environment = environment;
     }
 
-    load(): Record<string, string> {
+    load(): Record<string, unknown> {
         const baseFile: Record<string, unknown> = this.parser(fs.readFileSync(this.path, "utf8"));
         let envFile: Record<string, unknown> = {};
 
@@ -29,11 +54,9 @@ export abstract class BaseFileStrategy implements FileStrategy {
             envFile = this.parser(fs.readFileSync(this.path, "utf8"));
         }
 
-
-        // TODO: This is wrong. It needs to be mapped to a key value like.
         return {
-            ...baseFile,
-            ...envFile,
+            ...this.mapToKeyValueObject(baseFile),
+            ...this.mapToKeyValueObject(envFile),
         };
     }
 }
