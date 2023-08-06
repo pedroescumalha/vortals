f053eb3ac52b71fdcf21cdd843346d9bbac69db5
Pedro Escumalha <pedrosimoes93@gmail.com>
2023-08-06 21:47:03 +0200

    Update vortals.ts

--- src/vortals.ts
+++ src/vortals.ts
@@ -1,4 +1,6 @@
-type VortalsConfigurationGuard<T> = (config: unknown) => config is T;
+import { CONFIG_DELIMITER } from "./utils";
+
+export type VortalsConfigurationGuard<T> = (config: unknown) => config is T;
 
 export interface VortalsStrategy {
     load(): Record<string, unknown>;
@@ -14,19 +16,62 @@ export interface VortalsConfiguration {
 // TODO: consider changing this to a Set
 const data: Array<Record<string, unknown>> = [];
 
-function get<T>(key: string, guard?: VortalsConfigurationGuard<T>): T | undefined {
-    for (const config of data) {
-        // TODO: missing fetching sections
-        if (key in config) {
-            if (guard && !guard(config[key])) {
-                throw new Error("Invalid configuration");
-            }
+function hasChildren(configKeys: string[], parentKey: string): boolean {
+    return configKeys.some((k) => k.startsWith(parentKey + CONFIG_DELIMITER));
+}
 
-            return config[key] as T;
+function isArray(configKeys: string[], parentKey: string): boolean {
+    return configKeys.every((k) => Number.isInteger(+k.substring((parentKey + CONFIG_DELIMITER).length)));
+}
+
+function filterProviderByKey(provider: Record<string, unknown>, configKey: string): Record<string, unknown> {
+    const filteredObj: Record<string, unknown> = {};
+
+    // TODO: this doesnt work. Ex key1 = a, and key2 = abc. These are two completely different keys.
+    for (const key in provider) {
+        if (key.startsWith(configKey)) {
+            filteredObj[key] = provider[key];
         }
     }
-    
+
+    return filteredObj;
+}
+
+function buildConfigurationObject(configKey: string): unknown {
+    for (const provider of data) {
+        const configs = filterProviderByKey(provider, configKey);
+        const filteredKeys = Object.keys(configs);
+
+        if (!filteredKeys) {
+            continue;
+        }
+
+        if (hasChildren(filteredKeys, configKey)) {
+            if (isArray(filteredKeys, configKey)) {
+                return Object.values(configs);
+            }
+            
+            // TODO: it's an object
+        }
+
+        return configs[configKey];
+    }
+
     return undefined;
+}
+
+function get<T>(key: string, guard?: VortalsConfigurationGuard<T>): T | undefined {
+    const obj = buildConfigurationObject(key);
+
+    if (obj === undefined) {
+        return obj;
+    }
+
+    if (guard && !guard(obj)) {
+        throw new Error("Invalid configuration");
+    }
+
+    return obj as T;
 }
 
 function addStrategy(strategy: VortalsStrategy): void {
