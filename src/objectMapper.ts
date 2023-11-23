445a4d6628d41a7373cb4d1fb02212a204465554
Pedro Escumalha <pedrosimoes93@gmail.com>
2023-11-23 12:29:49 +0100

    abstract object assertions to utils

--- src/objectMapper.ts
+++ src/objectMapper.ts
@@ -1,14 +1,15 @@
+import { isArray, isRecord } from "./utils";
+
 function flat(target: Record<string, unknown>, delimiter: string): Record<string, unknown> {
     const res: Record<string, unknown> = {};
 
     function flatRecursively(object: Record<string, unknown> | unknown[], prevKey = ""): void {
         Object.entries(object).forEach(([key, value]) => {
             const isBuffer = Buffer.isBuffer(value);
-            const isObject = typeof value === "object" && value !== null;
 
             const newKey = prevKey ? prevKey + delimiter + key : key;
 
-            if (!isBuffer && isObject && Object.keys(value).length) {
+            if (!isBuffer && (isRecord(value) || isArray(value))) {
                 return flatRecursively(value as Record<string, unknown>, newKey);
             }
 
@@ -20,6 +21,7 @@ function flat(target: Record<string, unknown>, delimiter: string): Record<string
 
     return res;
 }
+
 
 export const objectMapper = {
     flat,
--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,10 @@
 export const CONFIG_PREFIX = "VORTALS";
 export const CONFIG_DELIMITER = "__";
+
+export function isRecord(obj: unknown): obj is Record<string, unknown> {
+    return typeof obj === "object" && !Array.isArray(obj) && obj !== null;
+}
+
+export function isArray(obj: unknown): obj is unknown[] {
+    return Array.isArray(obj);
+}
